74c42851a43a2eecb8f9084edb1666f4be1b3cc6
Tyler Cunnion <tyler.cunnion@gmail.com>
2009-03-13 16:45:47 -0400

    Update documentation

--- lib/sitemap_xml.rb
+++ lib/sitemap_xml.rb
@@ -19,6 +19,29 @@ module SitemapXml
     #
     #
     # ==== Parameters
+    # * <tt>:only</tt> - Map only the given actions
+    # * <tt>:except</tt> - Map all actions except the given actions
+    # * <tt>:include</tt> - Map the given additional actions. If you have
+    #   static actions with only a view and no corresponding method in the
+    #   controller, use this to tell the map about them.
+    # * <tt>:dynamic</tt> - Tells the map that the given actions are dynamic;
+    #   i.e., they take a parameter (usually <tt>id</tt>) to display an object.
+    #   This can take a hash or several hashes of the form 
+    #   <tt>:action_name => {options}</tt> to override the global default
+    #   options.
+    # * <tt>:model</tt> - Specify the model if it cannot be inferred from
+    #   the controller name.
+    # * <tt>:param</tt> - Specify the param to use if not <td>id</td>.
+    # * <tt>:conditions</tt> - Specify <tt>find()</tt> conditions for objects
+    #   attached to dynamic actions.
+    # * <tt>:collection</tt> - As an alternative to grabbing objects from the
+    #   database, you may specify a collection to use instead.
+    #
+    # ==== Examples
+    #   enable_sitemap :except => :post_result
+    #   enable_sitemap :dynamic => :show
+    #   enable_sitemap :dynamic => {:show => { :param => :year, :collection => (2000..Date.today.year).collect }}
+    #   enable_sitemap :resource, :include => :some_other_method
 
     def enable_sitemap(*args)
       sitemapped_actions = []
